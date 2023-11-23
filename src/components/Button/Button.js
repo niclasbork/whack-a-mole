590f50d7030145714aa91396eaa77155da5486a9
niclasbork <mr.niclas@web.de>
2023-11-23 09:15:02 +0100

    first commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Style.scss";
+
+const Button = () => {
+    return (
+        <button>
+            Start Game
+        </button>
+    ); 
+};
+
+export default Button;
